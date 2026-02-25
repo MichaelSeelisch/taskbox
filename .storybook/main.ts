be01a0d00074cf204573845aac32fc35b76b157e
Michael Seelisch <info@michaelseelisch.de>
2026-02-25 11:48:55 +0100

    built the task component

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,18 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
-  addons: [
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
-    "@storybook/addon-vitest",
-  ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
+	stories: ["../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
+	staticDirs: ["../public"],
+	addons: [
+		"@chromatic-com/storybook",
+		"@storybook/addon-docs",
+		"@storybook/addon-vitest",
+		"@storybook/addon-a11y"
+	],
+	framework: {
+		name: "@storybook/react-vite",
+		options: {}
+	}
 };
+
 export default config;
